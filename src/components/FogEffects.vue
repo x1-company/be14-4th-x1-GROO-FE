0f3d9d050a77dd3c5313439868d8b254a12a8ea9
JK-LEE98 <abc9541@gmail.com>
2025-05-02 11:35:05 +0900

    🔨 fix: 황사 날씨 수정

--- src/components/FogEffects.vue
+++ src/components/FogEffects.vue
@@ -1,53 +1,79 @@
 <template>
-    <div class="container">
-      <img
-        class="fog"
-        src="https://64.media.tumblr.com/224f9198d5d88a5e92e43f5ef4f7a592/139ff9bb70edd708-66/s540x810/0edbf6d586b6b4c85c5bd61569992f036c21191b.png"
-        alt="fog"
-      />
+  <div class="fog-wrapper">
+    <div id="fog-layer-01" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
     </div>
-  </template>
-  
-  <script setup>
-  // 이 컴포넌트는 별도 스크립트 로직이 필요 없습니다.
-  </script>
-  
-  <style scoped>
-  html,
-  body {
-    height: 100%;
-    margin: 0;
-    background-color: #000;
-    overflow: hidden;
+    <div id="fog-layer-02" class="fog">
+      <div class="image01"></div>
+      <div class="image02"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// No script logic needed
+</script>
+
+<style scoped>
+.fog-wrapper {
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  pointer-events: none;
+  z-index: 1000;
+  filter: blur(1px) brightness(0.99);
+}
+
+#fog-layer-01, #fog-layer-02 {
+  height: 100%;
+  position: absolute;
+  width: 200%;
+}
+
+#fog-layer-01 .image01, #fog-layer-01 .image02,
+#fog-layer-02 .image01, #fog-layer-02 .image02 {
+  float: left;
+  height: 100%;
+  width: 50%;
+  background: url("https://64.media.tumblr.com/224f9198d5d88a5e92e43f5ef4f7a592/139ff9bb70edd708-66/s540x810/0edbf6d586b6b4c85c5bd61569992f036c21191b.png") center center/cover no-repeat;
+}
+
+#fog-layer-01 {
+  animation: fog-move-1 25s linear infinite;
+  opacity: 0.4;
+}
+
+#fog-layer-02 {
+  animation: fog-move-2 20s linear infinite;
+  opacity: 0.3;
+}
+
+@keyframes fog-move-1 {
+  from {
+    transform: translateX(0);
   }
-  
-  .container {
-    display: flex;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 120%;
-    pointer-events: none;
+  to {
+    transform: translateX(-50%);
   }
-  
-  .fog {
-    opacity: 1;
-    min-width: 5000px;
-    position: relative;
-    animation-name: fog;
-    animation-duration: 3s;
-    animation-direction: alternate;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
+}
+
+@keyframes fog-move-2 {
+  from {
+    transform: translateX(-10%);
   }
-  
-  @keyframes fog {
-    from {
-      right: 0px;
-    }
-    to {
-      right: 50px;
-    }
+  to {
+    transform: translateX(-60%);
   }
-  </style>
+}
+
+@media (max-width: 767px) {
+  #fog-layer-01 .image01, #fog-layer-01 .image02,
+  #fog-layer-02 .image01, #fog-layer-02 .image02 {
+    width: 100%;
+  }
+}
+</style>
   
